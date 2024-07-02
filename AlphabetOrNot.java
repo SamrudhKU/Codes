34133c733bacd78b4e194c4271181d4e7f01388c
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-02 15:17:20 +0530

    solved

--- AlphabetOrNot.java
+++ AlphabetOrNot.java
@@ -1,6 +1,4 @@
 // ASCII value for upperCase alphabet is between 65 to 90
-
-
 import java.util.Scanner;
 
 public class AlphabetOrNot {
@@ -10,7 +8,10 @@ public class AlphabetOrNot {
         char ch = s.next().charAt(0);
         ch = Character.toUpperCase(ch);
         int ascii = (char)ch;
-        if (ascii>=65 && ascii<=90) {
+
+        // it uses assci values
+        // if (ascii>=65 && ascii<=90) {
+            if(ch>='A' && ch<='Z') {
             System.out.println(ch+": It is alphabet");
         } else {
             System.out.println(ch+": It is not alphabet");
