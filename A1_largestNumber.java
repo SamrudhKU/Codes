32b40835e44988a9c3f05de1e2073dd1155331a8
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-02 22:50:25 +0530

    solved

--- A1_largestNumber.java
+++ A1_largestNumber.java
@@ -4,13 +4,19 @@ public class A1_largestNumber {
             12, 34, 54, 53, 23, 23
         };
 
-        int max = arr[0];
+        int max = arr[0], min = arr[0];
+        
         for (int i = 0; i<arr.length; i++){
             if (arr[i]> max) {
                 max = arr[i];
+            } 
+            if(arr[i]<min){
+                min = arr[i];
             }
+
         }
 
-        System.out.println(max);
+        System.out.println("Maximum Element: "+max);
+        System.out.println("Minimum Element: "+min);
     }
 }
