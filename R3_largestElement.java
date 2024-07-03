d1e18007531cf5a8eec83421293105770eb1527c
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-03 12:45:07 +0530

    solved

--- R3_largestElement.java
+++ R3_largestElement.java
@@ -1,6 +1,6 @@
 public class R3_largestElement {
+
     public static int largeElement(int arr[], int i, int max){
-        
         if (arr.length==1) {
             return arr[arr.length-1];
         }
@@ -12,11 +12,24 @@ public class R3_largestElement {
         }
         return largeElement(arr, i+1, max);
     }
+
+    private static int lElement(int[] arr, int j) {
+        if(j == 1)
+            return arr[0];
+        
+        return Math.min(arr[j-1], lElement(arr, j-1));
+    }
+
     public static void main(String[] args) {
         int arr[] = new int[]{
             12, 34, 54, 53, 23, 23
         };
         int i = 0, max = 0;
-        System.out.println(largeElement(arr, i, max));
+        System.out.println("Largest Element "+largeElement(arr, i, max));
+
+        int j = arr.length;
+        System.out.println("Smallest Element "+lElement(arr, j));
     }
+
+   
 }
