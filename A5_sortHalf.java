c66732de28e4dcf8a445fa58d14cf203b50d766a
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-03 20:14:57 +0530

    solved

---
+++ A5_sortHalf.java
@@ -0,0 +1,41 @@
+// This program is to print half a array in acceding ordre and half arrya in decending order
+public class A5_sortHalf {
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 5, 4, 1, 2, 3 };
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        // It will sort the second half of the array in decending order
+        // int n = (arr.length)/2;
+        // for (int i =n; i<arr.length; i++) {
+        //     for (int j = i; j < arr.length; j++) {
+        //         if(arr[i]<arr[j]){
+        //             int temp = arr[i];
+        //             arr[i] = arr[j];
+        //             arr[j] = temp;
+        //         }   
+        //     }
+        // }
+        
+        // printing first half of the array
+        for (int i = 0; i < arr.length/2; i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+        for (int i = arr.length-1; i >= arr.length/2; i--) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+
+
+
