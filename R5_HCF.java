c3d06cece4e3e44a6a21754d3eb7f447af6b33d1
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-03 17:51:13 +0530

    solved

--- R5_HCF.java
+++ R5_HCF.java
@@ -1,24 +1,24 @@
 public class R5_HCF {
 
-    // public static int hcf(int num1, int num2){    
-    //     if (num1<=0 || num2<=0) {
-    //         return num1>num2 ? num1 :num2;
-    //     }
-    //     if(num1 == num2)
-    //         return num1;
-    //     return hcf(num2, num1%num2);
-    // }
- 
-    public static int hcf(int num1, int num2){
+    public static int hcf(int num1, int num2){    
         if (num1<=0 || num2<=0) {
             return num1>num2 ? num1 :num2;
         }
         if(num1 == num2)
             return num1;
-        if(num1>num2)
-            hcf(num1-num2, num2);
-        return hcf(num1, num2-num1);
+        return hcf(num2, num1%num2);
     }
+ 
+    // public static int hcf(int num1, int num2){
+    //     if (num1<=0 || num2<=0) {
+    //         return num1>num2 ? num1 :num2;
+    //     }
+    //     if(num1 == num2)
+    //         return num1;
+    //     if(num1>num2)
+    //         return hcf(num1-num2, num2);
+    //     return hcf(num1, num2-num1);
+    // }
     public static void main(String[] args) {
         int num1 = 12;
         int num2 = 18;
