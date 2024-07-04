13ecb2c561c20736c26b604137d7b2b8b0960d0c
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-04 16:32:27 +0530

    solved

---
+++ R6_LCM.java
@@ -0,0 +1,29 @@
+public class R6_LCM {
+
+    // public static int gcdOfNum(int n1, int n2){
+    //      if(n1<=0)
+    //         return n2;
+    //     if(n2<=0)
+    //         return n1;
+    //     if(n1 == n2) {
+    //         return n1;
+    //     }
+    //     return gcdOfNum(n2, n1%n2);
+    // }
+
+    public static int  gcdOfNum(int n1, int n2){
+        return n2==0?n1:gcdOfNum(n2, n1%n2);
+    }
+
+    public static int lcmOfNum(int n1, int n2) {
+        if (n1 == n2) {
+            return gcdOfNum(n1, n2);
+        }
+
+        return (n1*n2)/gcdOfNum(n1, n2);
+    }
+    public static void main(String[] args) {
+        int num1 = 0, num2 = 36;
+        System.out.println(lcmOfNum(num1, num2));
+    }
+}
