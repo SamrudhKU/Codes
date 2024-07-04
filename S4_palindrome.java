c3527083f47346d106efc85bf9f932751b52989c
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-04 15:48:00 +0530

    solved

---
+++ S4_palindrome.java
@@ -0,0 +1,33 @@
+public class S4_palindrome {
+
+    // public static boolean isPalindrome(String str) {
+    //     for (int i = 0; i < str.length()/2; i++) {
+    //         if (str.charAt(i) == str.charAt(str.length()-1-i)) {
+    //             return true;
+    //         } 
+    //     }
+    //     return false;
+    // }
+
+    public static boolean isPalindrome(String str, int i){
+        if (str.charAt(i) == str.charAt(str.length()-1-i)) {
+            return true;
+        } 
+        isPalindrome(str, i+1);
+
+        return false;
+        
+    }
+    public static void main(String args[]){
+        String str = "Samrudh";
+
+        
+        
+        if (isPalindrome(str, 0)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+    }
+}
