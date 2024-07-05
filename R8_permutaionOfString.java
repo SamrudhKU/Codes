46a3db2b114151da56dc3958f6b3727370c5bb06
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-05 11:01:30 +0530

    solutioned

--- R8_permutaionOfString.java
+++ R8_permutaionOfString.java
@@ -1,18 +1,21 @@
 public class R8_permutaionOfString {
 
-    public static void permut(String str, String ans) {
-        if(str.length()==0) {
+    // Recursion is mainly help for backtracking the problems
+    public static void permute(String str, String ans) {
+        if(str.length()==0){
             System.out.print(ans+" ");
-            return ;
+            return; 
         }
-        for (int i = 0; i < str.length(); i++) {
+        // each the loop will start at zero until it return the value of the function call
+        for(int i = 0; i<str.length(); i++) {
             char ch = str.charAt(i);
-            String r = str.substring(0, i) + str.substring(i+1);
-            permut(r, ans+ch);
+            String r = str.substring(0,i) + str.substring(i+1);
+            // Hear it will return the substring 
+            permute(r, ans+ch);
         }
     }
-    public static void main(String[] args) {
-        String str = "abc";
-        permut(str, "");
+    public static void main (String args[]){
+        String str = "abcd";
+        permute(str, "");
     }
 }
