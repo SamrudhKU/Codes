3ea48c7e8a0388aa4ed05eba35ae2eac0b0eab23
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-05 10:52:42 +0530

    solutioned

---
+++ A6_frequencyOfElement.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class A6_frequencyOfElement {
+    public static void main(String[] args) {
+        int arr[] = new int[] { 1, 1, 5, 1, 2, 36, 4, 88, 36, 88, 88 };
+        boolean[] visited = new boolean[arr.length];
+        Arrays.fill(visited, false);
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i] == true) {
+                continue;
+            }
+            int count = 0;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+            System.out.println(arr[i] + " Occurs " + count + " times");
+        }
+
+    }
+}
