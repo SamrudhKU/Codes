9c0b108acf7f08a959d63964719373aa2688c69b
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-05 16:27:49 +0530

    solved

---
+++ A8_longestPalindrome.java
@@ -0,0 +1,29 @@
+public class A8_longestPalindrome {
+    public static void main(String[] args) {
+        int arr[] = new int[]{1,12,1221,568,897,555};
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (isPalindrome(arr[i])) {
+                if (max < arr[i]) {
+                    max = arr[i];
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+    static boolean isPalindrome(int num) {
+        int temp = num;
+        int rev = 0;
+        while (temp>0) {
+            int last =  temp%10 ;
+            rev = (rev*10)+last;
+            temp/=10;
+        }
+        if (num == rev) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
