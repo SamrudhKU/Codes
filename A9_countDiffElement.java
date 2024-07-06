7fce3a60e207b8dbc34bd1d51f697f5b7f666201
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-06 18:54:00 +0530

    solved

--- A9_countDiffElement.java
+++ A9_countDiffElement.java
@@ -1,11 +1,13 @@
+// It shows the count of  differnt element and the reapting element
 import java.util.Arrays;
 
 public class A9_countDiffElement {
     public static void main(String args[]){
-        int arr[] = new int[]{1,1,3,3,2};
+        int arr[] = new int[]{1,16,7,8,8,2,4,4,1,54,3,3,2};
         boolean visited[] = new boolean[arr.length];
         Arrays.fill(visited, false); 
         int count = 0;
+        System.out.println("The repating elements are:");
         for (int i = 0; i < arr.length; i++) {
             if(visited[i]==true){
                 continue;
@@ -15,10 +17,12 @@ public class A9_countDiffElement {
                 
                 if (arr[i] == arr[j]) {
                     visited[j] = true;
+                    System.out.print(arr[i]+" ");
                 }
             }
             count++;
         }
-        System.out.println(count);       
+        System.out.println();
+        System.out.println("The count of different element is: "+count);       
     }
 }
