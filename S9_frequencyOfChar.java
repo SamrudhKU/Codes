a9b8f30ecf61b74ca9d9fac0b92f4f49e954974d
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-08 20:55:49 +0530

    solved

---
+++ S9_frequencyOfChar.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class S9_frequencyOfChar {
+    public static void main(String[] args) {
+        String str = "Sammus";
+        String temp = str;
+        str = str.toUpperCase();
+        char ch[] = new char[str.length()];
+        ch = str.toCharArray();
+        int freq[] = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+
+            freq[i] = 1;
+            for (int j = i + 1; j < str.length(); j++) {
+                if (ch[i] == ch[j]) {
+                    freq[i]++;
+                    ch[j] = '0';
+                }
+            }
+        }
+
+        for (int i = 0; i < freq.length; i++) {
+            if (ch[i] != '0')
+                System.out.println(temp.charAt(i) + ":" + freq[i]);
+        }
+
+        
+        // using the boolena array to find visited
+
+        // boolean visited[] = new boolean[str.length()];
+        // Arrays.fill(visited, false);
+        // for (int i = 0; i < str.length(); i++) {
+        // int count = 0;
+        // if (visited[i] == true) {
+        // continue;
+        // }
+        // for (int j = i; j < str.length(); j++) {
+        // if(str.charAt(i) == str.charAt(j)){
+        // count++;
+        // visited[j] = true;
+        // }
+        // }
+        // System.out.println(temp.charAt(i)+":"+count);
+        // }
+    }
+}
