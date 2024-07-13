5cb983534a4a039ff1247afa11611802b2c8e67a
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-13 18:14:05 +0530

    solved

---
+++ S11_replaceSubString.java
@@ -0,0 +1,23 @@
+class S11_replaceSubString {
+    public static void main(String[] args) {
+        String original  = "Hello World";
+        String toReplace = "World";
+        String toAdd = "Java";
+        String modifiedString = "";
+
+        String[] originalArray = original.split(" ");
+        for (int i = 0; i < originalArray.length; i++) {
+            if (originalArray[i].equals(toReplace)) {
+                originalArray[i] = toAdd;
+            }
+        }
+
+
+        // if (!original.contains(toReplace)) {
+        //     System.out.println("Replace string does'nt match");
+        // } else {
+        //     modifiedString = original.replace(toReplace,toAdd);
+        // }
+        // System.out.println(modifiedString);
+    }
+}
