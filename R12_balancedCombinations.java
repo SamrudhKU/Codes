e64c2fa97cb552d681c7afc2595c873a909322d5
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-15 19:51:38 +0530

    solutioned

--- R12_balancedCombinations.java
+++ R12_balancedCombinations.java
@@ -1,21 +1,24 @@
-
+// 
 public class R12_balancedCombinations {
     public static void patterCombanition(int n, int open, int close, String str) {
 
         if (open == n && close == n) {
             System.out.println(str);
-            // return str;
         }
         if (open < n)
             patterCombanition(n, open + 1, close, str + "{");
         if (close < open)
             patterCombanition(n, open, close + 1, str + "}");
-
     }
 
     public static void main(String args[]) {
         int n = 3;
         patterCombanition(n, 0, 0, " ");
-
+        // The following recursion are important 
+        // 1.  p(3,0,0," ");  prints  {{{}}}
+        // 2.  p(3,2,0, {{);  prints  {{}{}}
+        // 3.  p(3,2,1, {{}); prints  {{}}{}
+        // 4.  p(3,1,0, {);   prints  {}{{}}
+        // 5.  p(3,1,1,{});   print   {}{}{}
     }
 }
