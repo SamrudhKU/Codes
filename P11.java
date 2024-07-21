ca6ecfc7ed07c5ae76745e47317ac076162a3e96
SamrudhKU <samrudhkugowda@gmail.com>
2024-07-21 19:51:38 +0530

    solved

---
+++ P11.java
@@ -0,0 +1,10 @@
+public class P11 {
+    public static void main(String argsp[]){
+        for (int i = 3; i <= 7; i++) {
+            for (int j = 3; j <=i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
