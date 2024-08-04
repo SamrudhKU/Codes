69f5b79e11b11ffd794869085a3a22da2e33b74e
SamrudhKU <samrudhkugowda@gmail.com>
2024-08-04 18:02:03 +0530

    solved

---
+++ l2.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class l2 {
+
+    public static void replaceZeros(int arr[]){
+        
+    }
+    public static void main(String args[]){
+        // int arr[] = new int[]{0,1,0,3,12};
+        int arr[] = new int[]{0,0,1};
+
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        System.out.println(index);
+        while (index<arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+        
+        System.out.println(Arrays.toString(arr));
+
+
+        
+    }
+}
