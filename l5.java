2c1f388adcbde2dc239fabb9d171b7a158946257
SamrudhKU <samrudhkugowda@gmail.com>
2024-08-14 18:41:01 +0530

    solved

---
+++ l5.java
@@ -0,0 +1,16 @@
+public class l5 {
+    public static void main(String args[]){
+        // int arr[] = new int[]{7,1,5,3,6,4};
+        // int arr[] = new int[]{7,6,4,3,1};
+        int arr[] = new int[]{1,2,3,4,5};
+        
+        int target = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i]<arr[i+1]) {
+                target+= arr[i+1]-arr[i];
+            }
+        }
+
+        System.out.println(target);
+    }
+}
