dffb4560127cdba789c520a008ad1159cfc88196
SamrudhKU <samrudhkugowda@gmail.com>
2024-08-22 11:55:14 +0530

    solved

--- DS_BinarySearch.java
+++ DS_BinarySearch.java
@@ -1,19 +1,49 @@
-public class DSA_BinarySearch{
-    public static void main(String args[]){
-        int arr[] = new int[]{1,2,3,4,5,6};
-        int start = 0, end = arr.length-1;
-        int target = 55;
-        int mid = 0; 
-        while (start<=end) {
-            mid = (start + end) /2;
+public class DS_BinarySearch {
+
+    // Time complexity for 
+    public static void main(String args[]) {
+
+        int arr[] = new int[] { 1, 2, 3, 4, 5, 6 };
+        int target = 2;
+
+        // int result = binarySearch(arr, target);
+        int result = binarySearch(arr, target, 0, arr.length-1);
+        if (result != -1) {
+            System.out.println("Element is found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+
+
+    // This is a recursive method
+    private static int binarySearch(int[] arr, int target, int start, int end) {
+        if (start>end) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
             if (arr[mid] == target) {
-                System.out.println(arr[mid]);
-                break;
-            } else if (target>arr[mid]){
+                return mid;
+            } else if (target > arr[mid]) {
+               return binarySearch(arr, target, mid+1, end);
+            } else {
+               return binarySearch(arr, target, start, mid-1);
+            }
+  
+    }
+
+    private static int binarySearch(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target > arr[mid]) {
                 start = mid + 1;
             } else {
-                end = mid -1;
+                end = mid - 1;
             }
         }
+        return -1;
     }
 }
