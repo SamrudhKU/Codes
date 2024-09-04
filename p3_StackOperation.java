af9b723198a01c2856edff19360e9918fba7eed3
SamrudhKU <samrudhkugowda@gmail.com>
2024-09-04 16:38:11 +0530

    solved

--- p3_StackOperation.java
+++ p3_StackOperation.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 public class p3_StackOperation {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
-        final int max = 5;
+        int max = 2;
         int arr[] = new int[max];
         int top = -1;
         while (true) {
-            System.out.println("1>push 2>pop 3>peak 4>show 5>exit");
+            System.out.println("1>push 2>pop 3>isPresent 4>show 5>exit");
             switch (s.nextInt()) {
                 case 1:
-                    top = push(s, arr, top);
+                    arr = push(s, arr, top);
+                    top++;
                     break;
                 case 2:
                     top = pop(top, arr);
                     break;
                 case 3:
-                    peak(s, top, arr);
+                    isPresent(s, top, arr);
                     break;
                 case 4:
                     show(top, arr);
@@ -27,21 +28,21 @@ public class p3_StackOperation {
                     break;
                 default:
                     System.out.println("Invalid Choice");
-                    ;
+
             }
-            
+
         }
     }
 
     private static void show(int top, int arr[]) {
         for (int i = top; i >= 0; i--) {
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
 
     }
 
-    private static void peak(Scanner s, int top, int arr[]) {
+    private static void isPresent(Scanner s, int top, int arr[]) {
         System.out.println("Enter the search Element ");
         int value = s.nextInt();
         boolean status = false;
@@ -54,29 +55,50 @@ public class p3_StackOperation {
                 status = false;
             }
         }
-        if (status==false) {
+        if (status == false) {
             System.out.println("Element is not found");
-        } 
+        }
     }
 
     private static int pop(int top, int arr[]) {
-        if (top<0) {
+        if (top < 0) {
             System.out.println("Underflow");
         } else {
+            arr[top] = 0;
             top--;
+            if (top>0) {
+                arr = shirnk(arr, top);
+            }
+
         }
         return top;
     }
 
-    private static int push(Scanner s, int arr[], int top) {
-        if (top < arr.length-1) {
-            System.out.println("Enter the value: ");
-            int value = s.nextInt();
-            top++;
-            arr[top] = value;
-        } else {
-            System.out.println("Stack overflow");
+    private static int[] shirnk(int arr[], int top) {
+        if (top <= (arr.length / 2) / 2) {
+            int capacity = top;
+            int[] newStatck = new int[capacity];
+            System.arraycopy(arr, 0, newStatck, 0, capacity);
+            return newStatck;
         }
-        return top;
+        return arr;
+    }
+
+    private static int[] push(Scanner s, int arr[], int top) {
+        if (top >= arr.length - 1) {
+            arr = expand(arr);
+        }
+        System.out.println("Enter the value: ");
+        int value = s.nextInt();
+        top++;
+        arr[top] = value;
+        return arr;
+    }
+
+    public static int[] expand(int arr[]) {
+        int capacity = arr.length;
+        int dynamicStack[] = new int[capacity * 2];
+        System.arraycopy(arr, 0, dynamicStack, 0, capacity);
+        return dynamicStack;
     }
 }
